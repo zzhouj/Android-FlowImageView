0335fcc107c846fcda374fd0a276e78a30d13652
justin <zzhouj@yahoo.com>
2014-01-17 12:05:03 +0800

    test activity no title

--- FlowImageViewTest/src/com/coco/flowimageview/test/FlowImageViewTestActivity.java
+++ FlowImageViewTest/src/com/coco/flowimageview/test/FlowImageViewTestActivity.java
@@ -2,6 +2,7 @@ package com.coco.flowimageview.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.coco.flowimageview.FlowImageView;
 
@@ -11,6 +12,7 @@ public class FlowImageViewTestActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.flow_image_view_test);
 
